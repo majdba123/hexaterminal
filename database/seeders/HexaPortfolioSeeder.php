9be204c6876a072f73b4823447e74ffd3496d3ba
Yusuf Mohammad Jojeh <107572302+YusufJojeh@users.noreply.github.com>
2026-08-24 00:59:04 +0300

    fix: store Yusuf portrait with matching webp path

--- database/seeders/HexaPortfolioSeeder.php
+++ database/seeders/HexaPortfolioSeeder.php
@@ -32,7 +32,7 @@ class HexaPortfolioSeeder extends Seeder
             'specialization' => $data['specialization'], 'expertise' => $data['expertise'],
             'languages' => $data['languages'], 'location' => $data['location'],
             'email' => null, 'phone' => null,
-            'photo' => $photos->sync('yusuf-jojeh.webp', $data['photo']),
+            'photo' => $photos->sync('yusuf-jojeh.webp', 'team/yusuf-jojeh.webp'),
             'photo_alt' => $data['photo_alt'], 'github_url' => $data['github_url'],
             'linkedin_url' => $data['linkedin_url'], 'cv_file' => null,
             'is_published' => true, 'publication_consent' => true,
